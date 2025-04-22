936e6045879e7b6994bcf5d5b976adf25e4b273d
Avandor <avandor404@gmail.com>
2025-04-22 11:51:40 +1000

    holy translations batman. setting marketcrate to render 4 evenly spaced items. 1 hour per line of code :upside-down-smile:

--- src/client/java/com/avandortools/simplemarket/rendering/blockentity/MarketCrateBlockEntityRenderer.java
+++ src/client/java/com/avandortools/simplemarket/rendering/blockentity/MarketCrateBlockEntityRenderer.java
@@ -12,6 +12,7 @@ import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RotationAxis;
+import org.joml.Matrix4f;
 
 public class MarketCrateBlockEntityRenderer implements BlockEntityRenderer<MarketCrateBlockEntity> {
     private final ItemRenderer itemRenderer;
@@ -26,17 +27,34 @@ public class MarketCrateBlockEntityRenderer implements BlockEntityRenderer<Marke
             ItemStack itemStack = entity.getFirstNonempty();
             renderItemOnBlock(itemStack, matrices, vertexConsumers, light, overlay);
         }
-        System.out.println("crate ItemStack: " + entity.getItems());
+//        System.out.println("crate ItemStack: " + entity.getItems());
     }
 
     private void renderItemOnBlock(ItemStack itemStack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        // Adjust the position of the item on the block
-        matrices.push();
-        matrices.translate(0.5, 1.0, 0.5); // This centers the item on top of the block
+        double offsetMargin = 2/16.0 + (12.0/16.0)/4;
+        double itemSize = 10.0/16.0; //half because we scale 0.5?
+        double offsetInnerX = offsetMargin;
+        double offsetInnerZ = offsetMargin - itemSize/8;
+        double offsetOuterX = 1 - offsetMargin;
+        double offsetOuterZ = offsetOuterX - itemSize/8;
 
-        // Render the item
-        itemRenderer.renderItem(itemStack, ModelTransformationMode.GROUND, light, overlay, matrices, vertexConsumers, null, 0);
+        double[][] positions = {
+                {offsetInnerX, offsetInnerZ},
+                {offsetInnerX, offsetOuterZ},
+                {offsetOuterX, offsetInnerZ},
+                {offsetOuterX, offsetOuterZ}
+        };
+        for (double[] pos : positions) {
+            matrices.push();
 
-        matrices.pop();
+
+            matrices.translate(pos[0], 2.0/16.0, pos[1]); //TODO: change 1 back to 1/16 when fin debugging
+            matrices.scale(0.5f, 0.1f, 0.5f);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
+
+            itemRenderer.renderItem(itemStack, ModelTransformationMode.GROUND, light, overlay, matrices, vertexConsumers, null, 0);
+
+            matrices.pop();
+        }
     }
 }
